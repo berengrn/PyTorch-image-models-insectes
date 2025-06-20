123751cd5d4c5c2a9942eec16390afcdfabd5ed9
beren <berengergranier@gmail.com>
2025-06-20 15:55:25 +0200

    loss qui marche !

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -11,30 +11,29 @@ from pathlib import Path
 from timm.loss import TaxaNetLoss,BuildDictionaries
 from timm.loss.taxanet_custom_loss import Build_H_Matrix
 
-hierarchy_csv = os.path.join(os.getcwd(),"hierarchy-test.csv")
-#niveau 1: 2 classes (0,1), niveau 2: 3 classes (2,3,4), niveau 3: 4 classes (5,6,7,9) (total:9)
+hierarchy_csv = os.path.join(os.getcwd(),"hierarchy_test.csv")
 
-parent_to_children,dict_especes,N = BuildDictionaries(hierarchy_csv)
+y_true = torch.tensor([[0,2,5,9],[0,2,6,10],[0,3,7,11]])
 
-y_true = torch.tensor([[[1.,0.,1.,0.,0.,1.,0.,0.,0.,0.],
-                        [1.,0.,1.,0.,0.,0.,1.,0.,0.,0.],
-                        [1.,0.,0.,1.,0.,0.,0.,1.,0.,0.],
-                        ]])
+y_correct = torch.tensor([[1.,0.,1.,0.,0.,1.,0.,0.,0.,1.,0.,0.], #résultat parfait
+                          [1.,0.,1.,0.,0.,0.,1.,0.,0.,0.,1.,0.],  
+                          [1.,0.,0.,1.,0.,0.,0.,1.,0.,0.,0.,1.]
+                        ])
 
-y_false_1 = torch.tensor([[[0.,1.,1.,0.,0.,1.,0.,0.,0.,0.], #grosses erreurs hiérachiques
-                        [0.,1.,1.,0.,0.,0.,1.,0.,0.,0.],
-                        [1.,0.,0.,1.,0.,0.,0.,1.,0.,0.],
-                        ]])
+y_false1 = torch.tensor([[1.,0.,0.,0.,1.,1.,0.,0.,0.,1.,0.,0.], #graves erreurs hiérarchiques
+                         [1.,0.,1.,0.,0.,0.,0.,1.,0.,0.,1.,0.],  
+                         [1.,0.,0.,1.,0.,0.,0.,1.,0.,0.,0.,1.]
+                        ])
 
-y_false_2 = torch.tensor([[[1.,0.,1.,0.,0.,0.,0.,0.,0.,1.], #petites erreurs hiérarchiques
-                        [1.,0.,1.,0.,0.,0.,0.,0.,0.,1.],
-                        [1.,0.,0.,1.,0.,0.,0.,1.,0.,0.],
-                        ]])
+y_false2 = torch.tensor([[1.,0.,1.,0.,0.,1.,0.,0.,0.,0.,0.,1.], #légères erreurs hiérarchiques
+                         [1.,0.,1.,0.,0.,0.,1.,0.,0.,0.,0.,1.],  
+                         [1.,0.,0.,1.,0.,0.,0.,1.,0.,0.,0.,1.]
+                        ])
 
-y_false_3 = torch.tensor([[[1.,0.,1.,0.,0.,0.,1.,0.,0.,0.],  #erreurs de prédictions mais hiérachie respectée
-                        [1.,0.,1.,0.,0.,1.,0.,0.,0.,0.],
-                        [1.,0.,0.,1.,0.,0.,0.,1.,0.,0.],
-                        ]])
+y_false3 = torch.tensor([[1.,0.,1.,0.,0.,0.,1.,0.,0.,0.,1.,0.], #erreurs de prédiction, mais respectant la hiérarchie
+                         [1.,0.,1.,0.,0.,1.,0.,0.,0.,1.,0.,0.],  
+                         [1.,0.,0.,1.,0.,0.,0.,1.,0.,0.,0.,1.]
+                        ])
 
 loss_fn = TaxaNetLoss(hierarchy_csv)
 
@@ -44,8 +43,8 @@ print(loss_fn(y_false1,y_true))
 print("loss pour une prédiction avec de légères erreurs hiérachiques")
 print(loss_fn(y_false2,y_true))
 
-print("loss pour une prédiction avec des erreurs de p^rédiction mais pas d'erreurs hiérarchiques")
+print("loss pour une prédiction avec des erreurs de prédiction mais pas d'erreurs hiérarchiques")
 print(loss_fn(y_false3,y_true))
 
 print("loss pour un résultat exact")
-print(loss_fn(y_true,y_true))
+print(loss_fn(y_correct,y_true))
--- timm/loss/TaxaNet_custom_loss.py
+++ timm/loss/TaxaNet_custom_loss.py
@@ -69,12 +69,15 @@ def BuildDictionaries(csv_path):
 
     parent_to_children = {k: list(v) for k, v in parent_to_children.items()}
 
-    return (parent_to_children,idx_all,(n_ordres + n_familles + n_genres + n_especes))
+    #les piquets indiquent ou s'arrête chaque partie du vecteur correspondant à une niveau hiérarchique
+    piquets = torch.cumsum(torch.tensor([0,n_ordres,n_familles,n_genres,n_especes]),0)
 
-def Build_H_Matrix(parent_to_children,N):
-    #H[i][j] == 1 signifie i parent de j
+    return (parent_to_children,idx_all,piquets)
+
+def Build_H_Matrix(parent_to_children,piquets):
+    #Construit H telle que H[i][j] == 1 ssi i est parent de j, 0 sinon
+    N = piquets[-1]
     H = np.zeros((N,N))
-
     for i in range(N):
         if i in parent_to_children:
             for j in parent_to_children[i]:
@@ -85,27 +88,44 @@ class TaxaNetLoss(nn.Module):
 
     def __init__(self,csv_path):
         super().__init__()
-        parent_to_children,_,N = BuildDictionaries(csv_path)
-        self.H = Build_H_Matrix(parent_to_children,N)
+        parent_to_children,_,self.piquets = BuildDictionaries(csv_path)
+        self.H = Build_H_Matrix(parent_to_children,self.piquets)
     
     def forward(self,y_pred,y_true):
         loss = 0.0
         sum = 0.0
         weights = [0.25, 0.25, 0.15, 0.1] #Les poids sont sans doute à re-tester pour notre dataset (pas les mêmes niveaux taxonomiques)
-        levels_pred = torch.unbind(y_pred,dim = 1)
+        N,C_total = y_pred.size()
+        nbLevels = len(self.piquets) - 1
+
+        """
+        N: Number of elements in the dataset
+        C_total: Number of classes, all hierarchical levels combined
+        nbLevels: Number of levels in the hierarchy
+        """
+
+        #arrays indiquant quels indices annuler pour ne garder que les classes k-ième niveau de la hiérarchie
+        levels_supports = [torch.tensor([int(i >= self.piquets[k -1] and i < self.piquets[k]) for i in range(C_total)]) for k in range(1,nbLevels+1)]
+
+        #tenseurs sur lesquels appliquer la cross-entropy loss:
+        levels_pred = torch.stack([ torch.stack([y_pred[j] * levels_supports[i] for j in range(N)])  for i in range(nbLevels)])
+
         levels_true = torch.unbind(y_true,dim = 1)
+        
         ce_fn = nn.CrossEntropyLoss()
-        for j in range(1,len(y_pred[0])):
-        #on itère d'abord le long des colonnes (prédictions pour les ordres, puis les familles, etc.)
+        for k in range(1,nbLevels):
+        #on itère d'abord sur chaque niveau hiérarchique
             sum = 0.0
-            for i in range(len(y_pred)):
-                sum += ((self.H[round(y_pred[i,j-1].item())][round(y_pred[i,j].item())] != 1)*np.e) #H[i][j] == 1 signifie i parent de j
-            sum += ce_fn(levels_pred[j],levels_true[j])
-            print(ce_fn(levels_pred[j],levels_true[j]))
-            sum *= weights[j]
+
+            for i in range(1,N):
+                sum += (self.H[torch.argmax(levels_pred[k-1][i])][torch.argmax(levels_pred[k][i])] == 0)*np.e
+            sum += ce_fn(levels_pred[k],levels_true[k])
+            sum *= weights[k]
             loss += sum
+
         return loss
 
 if __name__ == '__main__':
+    
     csv_path = os.path.join(os.pardir,"small-collomboles","hierarchy.csv")
     loss_fn = TaxaNetLoss(csv_path)
